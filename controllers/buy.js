9179fb42ada5471648111c6a0196d5dbf506e6ed
iamfarraz <farazanwar8@gmail.com>
2021-05-07 00:21:37 +0530

    testing changes 4.0

--- controllers/buy.js
+++ controllers/buy.js
@@ -11,8 +11,9 @@ export const handleBuy=(req,res,db)=>{
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
+    var time =  today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
     today = `${yyyy}-${mm}-${dd}`
-    var time =  `23:12:11`;
+    
     db.query(sql,(err,result)=>{
         if(err)throw err;
         console.log(result)
